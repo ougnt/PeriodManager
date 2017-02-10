ab2d52c576efe6ecc688344ac91d989ee7eeca85
Wacharin Tangseree <ougntnarak@gmail.com>
2017-02-10 09:14:11 +0700

    Version 73 : UI Help

--- src/com/ougnt/period_manager/activity/InitialActivity.java
+++ src/com/ougnt/period_manager/activity/InitialActivity.java
@@ -80,7 +80,7 @@ public class InitialActivity extends Activity {
     final int DisplayNewActionPanel = 0x80;
     final int DisplaySettingWizard = 0x100;
 
-    public static final int ApplicationVersion = 72;
+    public static final int ApplicationVersion = 73;
 
     // TODO : Change this to the real one
     // Live Env
